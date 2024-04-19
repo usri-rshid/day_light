2d6bfe4bf43978ae4af5a14deb4aa902860d0fa7
usri.rachid <usri.rachid@gmail.com>
2024-04-19 22:15:33 +0100

    modify js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-document.getElementById("test").innerHTML = " hello world";
+let menuToggle = document.querySelector(".menuToggle");
+let lightDark = document.querySelector(".lightDark");
+let body = document.querySelector("body");
+let video = document.querySelector("video");
+let dark = false;
+let navigation = document.querySelector(".navigation");
+
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+};
+
+lightDark.onclick = function () {
+  dark = !dark;
+  lightDark.classList.toggle("active");
+  body.classList.toggle("dark");
+
+  if (dark) {
+    video.setAttribute("src", "../night.mp4");
+  } else {
+    video.setAttribute("src", "../day.mp4");
+  }
+};
